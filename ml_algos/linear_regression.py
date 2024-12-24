ca9d9a63c1b88fa150793d80a5f0b7b49d6cff4b
Mahesh Babu <maheshchowdary973@gmail.com>
2024-12-23 22:36:34 -0500

    added docstrings to enhance readability

--- ml_algos/linear_regression.py
+++ ml_algos/linear_regression.py
@@ -4,13 +4,36 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
 class LinearRegression:
-    def __init__(self, lr = 0.001, n_iters = 1000):
+    """
+    Linear Regression model.
+
+    Attributes:
+        lr (float): Learning rate for gradient descent.
+        n_iters (int): Number of iterations for gradient descent.
+        weights (np.ndarray): Weights of the model.
+        bias (float): Bias term of the model.
+    """
+    def __init__(self, lr=0.001, n_iters=1000):
+        """
+        Initializes the LinearRegression model with learning rate and number of iterations.
+
+        Args:
+            lr (float): Learning rate for gradient descent. Default is 0.001.
+            n_iters (int): Number of iterations for gradient descent. Default is 1000.
+        """
         self.lr = lr
         self.n_iters = n_iters
         self.weights = None
         self.bias = None
 
     def fit(self, X, y):
+        """
+        Fits the LinearRegression model to the training data.
+
+        Args:
+            X (np.ndarray): Training data features.
+            y (np.ndarray): Training data labels.
+        """
         n_samples, n_features = X.shape
         self.weights = np.random.rand(n_features)
         self.bias = 0
@@ -25,10 +48,20 @@ class LinearRegression:
             self.bias -= self.lr * db
 
     def predict(self, X):
+        """
+        Predicts the labels for the given data.
+
+        Args:
+            X (np.ndarray): Data features.
+
+        Returns:
+            np.ndarray: Predicted labels.
+        """
         return np.dot(X, self.weights) + self.bias
     
 
 if __name__ == "__main__":
+    # Generate a dataset to test the LinearRegression implementation
     X, y = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=4)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
 
@@ -37,4 +70,3 @@ if __name__ == "__main__":
     y_pred = regressor.predict(X_test)
 
     print(f"The RMSE value on the test set is :{np.sqrt(mean_squared_error(y_test, y_pred))}")
-
