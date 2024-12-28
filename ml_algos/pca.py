0395a4f35234dfa1a9452cac9e015fcc09e712e2
Mahesh Babu <maheshchowdary973@gmail.com>
2024-12-28 17:11:39 -0500

    implementing PCA

---
+++ ml_algos/pca.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class PCA:
+    def __init__(self, n_components = None):
+        self.n_components = n_components
+    
+    def fit(self, X):
+        pass
+
+    def transform(self, X):
+        pass
+
+    def fit_transform(self, X):
+        pass
