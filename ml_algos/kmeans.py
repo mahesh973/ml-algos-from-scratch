598d994f8e8830ecbc3430c9d0b354c52bae339f
Mahesh Babu <maheshchowdary973@gmail.com>
2024-12-30 18:19:09 -0500

    added docstrings

--- ml_algos/kmeans.py
+++ ml_algos/kmeans.py
@@ -4,10 +4,48 @@ from sklearn.datasets import make_blobs
 np.random.seed(42)
 
 def euclidean_distance(x1, x2):
+    """
+    Compute the Euclidean distance between two points.
+
+    Parameters
+    ----------
+    x1 : ndarray of shape (n_features,)
+        The first point.
+
+    x2 : ndarray of shape (n_features,)
+        The second point.
+
+    Returns
+    -------
+    float
+        The Euclidean distance between `x1` and `x2`.
+    """
     return np.sqrt(np.sum((x1 - x2)**2))
 
 
 class KMeans:
+    """
+    K-Means clustering algorithm.
+
+    Parameters
+    ----------
+    k : int, default=5
+        The number of clusters.
+
+    max_iters : int, default=100
+        Maximum number of iterations for the clustering algorithm.
+
+    plot_steps : bool, default=False
+        If True, plot the steps of the algorithm at each iteration.
+
+    Attributes
+    ----------
+    clusters : list of list
+        Indices of samples in each cluster.
+
+    centroids : list of ndarray of shape (k, n_features)
+        Coordinates of the cluster centroids.
+    """
     def __init__(self,k = 5, max_iters = 100, plot_steps = False):
         self.k = k
         self.max_iters = max_iters
@@ -20,6 +58,19 @@ class KMeans:
         self.centroids = []
 
     def predict(self, X):
+        """
+        Fit the K-Means model and predict the cluster for each sample.
+
+        Parameters
+        ----------
+        X : ndarray of shape (n_samples, n_features)
+            Input data to cluster.
+
+        Returns
+        -------
+        ndarray of shape (n_samples,)
+            Cluster labels for each sample.
+        """
         self.X = X
         self.n_samples, self.n_features = X.shape
 
@@ -49,6 +100,19 @@ class KMeans:
         return self._get_cluster_labels(self.clusters)
 
     def _create_clusters(self, centroids):
+        """
+        Assign each sample to the nearest centroid.
+
+        Parameters
+        ----------
+        centroids : list of ndarray of shape (k, n_features)
+            Current centroids of the clusters.
+
+        Returns
+        -------
+        list of list
+            Indices of samples in each cluster.
+        """
         clusters = [[] for _ in range(self.k)]
 
         for idx, sample in enumerate(self.X):
@@ -57,11 +121,40 @@ class KMeans:
         return clusters
     
     def _closest_centroid(self, sample, centroids):
+        """
+        Identify the index of the nearest centroid to a given sample.
+
+        Parameters
+        ----------
+        sample : ndarray of shape (n_features,)
+            Sample for which the closest centroid is to be determined.
+
+        centroids : list of ndarray of shape (k, n_features)
+            Current centroids of the clusters.
+
+        Returns
+        -------
+        int
+            Index of the nearest centroid.
+        """
         distances = [euclidean_distance(sample, point) for point in centroids]
         closest_idx = np.argmin(distances)
         return closest_idx
     
     def _recompute_centroids(self, clusters):
+        """
+        Update centroids as the mean of samples assigned to each cluster.
+
+        Parameters
+        ----------
+        clusters : list of list
+            Indices of samples in each cluster.
+
+        Returns
+        -------
+        ndarray of shape (k, n_features)
+            Updated centroids.
+        """
         centroids = np.zeros((self.k, self.n_features))
         for cluster_idx, cluster in enumerate(clusters):
             cluster_mean = np.mean(self.X[cluster], axis = 0)
@@ -69,10 +162,39 @@ class KMeans:
         return centroids
 
     def _is_converged(self, old_centroids, new_centroids):
+        """
+        Check if the algorithm has converged.
+
+        Parameters
+        ----------
+        old_centroids : list of ndarray of shape (k, n_features)
+            Centroids from the previous iteration.
+
+        new_centroids : list of ndarray of shape (k, n_features)
+            Centroids from the current iteration.
+
+        Returns
+        -------
+        bool
+            True if centroids have not changed, otherwise False.
+        """
         distances = [euclidean_distance(a,b) for a,b in zip(old_centroids, new_centroids)]
         return sum(distances) == 0
 
     def _get_cluster_labels(self, clusters):
+        """
+        Generate cluster labels for each sample.
+
+        Parameters
+        ----------
+        clusters : list of list
+            Indices of samples in each cluster.
+
+        Returns
+        -------
+        ndarray of shape (n_samples,)
+            Cluster labels for each sample.
+        """
         labels = np.empty(self.n_samples)
         for cluster_idx, cluster in enumerate(clusters):
             for sample_idx in cluster:
@@ -80,6 +202,11 @@ class KMeans:
         return labels
     
     def plot(self):
+        """
+        Visualize the current clustering state.
+
+        Shows the clusters and centroids using a scatter plot.
+        """
         fig, ax = plt.subplots(figsize = (12,8))
 
         for i, index in enumerate(self.clusters):
