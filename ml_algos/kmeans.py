ba7f9cf72c5810488e8a66209ae4d8b016aa5d13
Mahesh Babu <maheshchowdary973@gmail.com>
2024-12-29 23:38:51 -0500

    implementing kmeans

---
+++ ml_algos/kmeans.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+class KMeans:
+    def __init__(self, k=5, max_iters = 1000):
+        self.k = k
+        self.max_iters = max_iters
+
+    def fit(self, X):
+        pass
+
+    def transform(self, X):
+        pass
+
