ce200d401cdfcc45632802a816fd29ff6bc64ca3
Mahesh Babu <maheshchowdary973@gmail.com>
2024-12-30 18:42:00 -0500

    implementing Decision Tree classifier....

---
+++ ml_algos/decision_tree.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def entropy(y):
+    hist = np.bincount(y)
+    ps = hist / len(y)
+    return -np.sum([p * np.log2(p) for p in ps if p > 0])
+
+class Node:
+    def __init__(self, feature = None, threshold = None, left = None, right = None, * ,value = None):
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.value = value
+
+    def is_leaf_node(self):
+        return self.value is not None
+
+class DecisionTreeClassifier:
+    def __init__(self):
+        pass
