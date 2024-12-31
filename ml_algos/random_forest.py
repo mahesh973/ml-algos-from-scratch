3bbb177cc5488bf53e378cb52e834527ace45c39
Mahesh Babu <maheshchowdary973@gmail.com>
2024-12-30 20:38:52 -0500

    refactored the code, added docstrings and testing logic

--- ml_algos/random_forest.py
+++ ml_algos/random_forest.py
@@ -8,6 +8,29 @@ from sklearn.metrics import accuracy_score
 
 
 def bootstrap_sample(X, y, n_features = 'sqrt'):
+    """
+    Generate a bootstrap sample from the dataset and select a subset of features.
+
+    Parameters
+    ----------
+    X : ndarray of shape (n_samples, n_features)
+        The feature matrix.
+
+    y : ndarray of shape (n_samples,)
+        The target labels.
+
+    n_features : int or str, default='sqrt'
+        The number of features to select for each tree. If 'sqrt', selects the 
+        square root of the total number of features.
+
+    Returns
+    -------
+    X_sample : ndarray of shape (n_samples, n_selected_features)
+        The feature matrix for the bootstrap sample with selected features.
+
+    y_sample : ndarray of shape (n_samples,)
+        The target labels for the bootstrap sample.
+    """
     n_samples = X.shape[0]
     n_features = X.shape[1]
     idxs = np.random.choice(n_samples, size = n_samples, replace=True)
@@ -15,24 +38,43 @@ def bootstrap_sample(X, y, n_features = 'sqrt'):
     return X[idxs, :][:,features], y[idxs]
 
 def most_common_label(y):
-        """
-        Determine the most common label in a set of labels.
+    """
+    Determine the most common label in a set of labels.
 
-        Parameters
-        ----------
-        y : ndarray of shape (n_samples,)
-            The target labels.
+    Parameters
+    ----------
+    y : ndarray of shape (n_samples,)
+        The target labels.
 
-        Returns
-        -------
-        int
-            The most common label.
-        """
-        counter = Counter(y)
-        return counter.most_common(1)[0][0]
+    Returns
+    -------
+    int
+        The most common label.
+    """
+    counter = Counter(y)
+    return counter.most_common(1)[0][0]
 
 
 class RandomForestClassifier:
+    """
+    A Random Forest classifier, which builds an ensemble of decision trees 
+    using bootstrapped datasets and feature subsampling.
+
+    Parameters
+    ----------
+    n_estimators : int, default=100
+        The number of trees in the forest.
+
+    min_samples_split : int, default=2
+        The minimum number of samples required to split an internal node.
+
+    max_depth : int, default=100
+        The maximum depth of the tree.
+
+    n_feats : int, optional, default=None
+        The number of features to consider when looking for the best split. 
+        If None, all features are considered.
+    """
     def __init__(self, n_estimators = 100, min_samples_split = 2, max_depth = 100, n_feats = None):
         self.n_estimators = n_estimators
         self.min_samples_split = min_samples_split
@@ -41,6 +83,17 @@ class RandomForestClassifier:
         self.trees = []
 
     def fit(self, X, y):
+        """
+        Fit the random forest classifier by training multiple decision trees on bootstrapped samples.
+
+        Parameters
+        ----------
+        X : ndarray of shape (n_samples, n_features)
+            The feature matrix.
+
+        y : ndarray of shape (n_samples,)
+            The target labels.
+        """
         self.trees = []
         for _ in range(self.n_estimators):
             tree = DecisionTreeClassifier(min_samples_split = self.min_samples_split,
@@ -52,6 +105,19 @@ class RandomForestClassifier:
             self.trees.append(tree)
 
     def predict(self, X):
+        """
+        Predict the class labels for the input samples by aggregating the predictions from all trees.
+
+        Parameters
+        ----------
+        X : ndarray of shape (n_samples, n_features)
+            The feature matrix for which predictions are to be made.
+
+        Returns
+        -------
+        y_pred : list of int
+            The predicted class labels for each sample in X.
+        """
         tree_preds = np.array([tree.predict(X) for tree in self.trees])
         tree_preds = np.swapaxes(tree_preds, 0, 1)
 
