89dc5bdf25b72683407577e9520618efd141f7c8
Mahesh Babu <maheshchowdary973@gmail.com>
2024-12-31 17:52:35 -0500

    added skeleton code for Adaboost

---
+++ adaboost.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class DecisionStump:
+
+    def __init__(self):
+        self.polarity = 1
+        self.feature_index = None
+        self.threshold = None
+        self.alpha = None
+
+    def predict(self, X):
+        pass
+
+class AdaBoost:
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
