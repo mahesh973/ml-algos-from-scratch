f9712b2c61c5e280493f7742b6bc0f85e9729749
Mahesh Babu <maheshchowdary973@gmail.com>
2024-12-30 19:56:16 -0500

    implemented the growing tree logic

--- ml_algos/decision_tree.py
+++ ml_algos/decision_tree.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections import Counter
 
 def entropy(y):
     hist = np.bincount(y)
@@ -17,5 +18,88 @@ class Node:
         return self.value is not None
 
 class DecisionTreeClassifier:
-    def __init__(self):
+    def __init__(self, min_samples_split=2, max_depth = 100, n_feats = None):
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
+        self.n_feats = n_feats
+        self.root = None
+
+    def fit(self, X, y): 
+        # Grow the tree
+        self.n_feats = X.shape[1] if not self.n_feats else min(self.n_feats, X.shape[1])
+        self.root = self._grow_tree(X, y)
+    
+    def _grow_tree(self, X, y, depth = 0):
+        n_samples, n_features = X.shape
+        n_labels = len(np.unique(y))
+
+        # 1. Stopping Criteria
+        if (depth >= self.max_depth
+            or n_labels == 1
+            or n_samples < self.min_samples_split):
+            leaf_value = self._most_common_label(y)
+            return Node(value=leaf_value)
+        
+        # 2. Continue growing the tree
+        feature_idxs = np.random.choice(n_features, self.n_feats, replace=False)
+
+        # Greedy Search
+        best_feature, best_threshold = self._best_criteria(X, y, feature_idxs)
+        left_idxs, right_idxs = self._split(X[:,best_feature], best_threshold)
+        left = self._grow_tree(X[left_idxs, :], y[left_idxs], depth + 1)
+        right = self._grow_tree(X[right_idxs, :], y[right_idxs], depth + 1)
+        return Node(best_feature, best_threshold, left, right)
+    
+    def predict(self, X):
+        # Traverse the tree
         pass
+
+    def _best_criteria(self, X, y, feature_idxs):
+        best_gain = -1
+        split_idx, split_threshold = None, None
+
+        for feature_idx in feature_idxs:
+            X_column = X[:, feature_idx]
+            thresholds = np.unique(X_column)
+
+            for threshold in thresholds:
+                gain = self._information_gain(y, X_column, threshold)
+
+                if gain > best_gain:
+                    best_gain = gain
+                    split_idx = feature_idx
+                    split_threshold = threshold
+
+        return split_idx, split_threshold
+
+    def _information_gain(self, y, X_column, split_threshold):
+        # 1. Calculate parent Entropy
+        parent = entropy(y)
+
+        # 2. Generate the split
+        left_idxs, right_idxs = self._split(X_column, split_threshold)
+
+        if len(left_idxs) == 0 or len(right_idxs) == 0:
+            return 0
+        
+        # 3. Calculate weighted average of child E
+        n = len(y)
+        n_l, n_r = len(left_idxs), len(right_idxs)
+        e_l, e_r = entropy(y[left_idxs]), entropy(y[right_idxs])
+        
+        child_entropy = (n_l / n) * e_l + (n_r / n) * e_r
+
+        # 4. Return Information gain
+        information_gain = parent - child_entropy
+        return information_gain
+
+    
+    def _split(self, X_column, split_threshold):
+        left_idxs = np.argwhere(X_column <= split_threshold).flatten()
+        right_idxs = np.argwhere(X_column > split_threshold).flatten()
+        return left_idxs, right_idxs
+
+    def _most_common_label(self, y):
+        counter = Counter(y)
+        return counter.most_common(1)[0][0]
+
